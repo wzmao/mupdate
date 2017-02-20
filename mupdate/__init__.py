2977ee3d7752757b4ed68dcf69d88d29748093f1
Wenzhi Mao <maowenzhi@yandex.com>
2017-02-20 15:29:57 +0800

    Add init file.

---
+++ mupdate/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Wenzhi Mao'
+__version__ = '0.0.1'
+
+__release__ = [int(x) for x in __version__.split('.')]
+del x
+__all__ = []
+
+from platform import system
+from sys import version_info
+
+_system = system()
+del system
+
+_PY3K = version_info[0] > 2
+_PY2K = not _PY3K
+del version_info
